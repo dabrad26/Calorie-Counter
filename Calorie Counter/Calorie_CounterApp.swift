fd66c5b776f8c31b1c7c3817595bdb4cf875527f
David Bradshaw <dabrad26@me.com>
2024-02-07 18:12:12 -0800

    Initial Commit

---
+++ Calorie Counter/Calorie_CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calorie_CounterApp.swift
+//  Calorie Counter
+//
+//  Created by David Bradshaw on 2/7/24.
+//
+
+import SwiftUI
+
+@main
+struct Calorie_CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
